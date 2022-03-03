fdf81443f1b045e383857558aea10123839b8748
subhransu-subudhi-87 <subhransu-subudhi87@gmail.com>
2022-03-03 21:50:29 +0530

    Service-Discovery Feature with Feign Client Implemented and pushed

--- student-microservice/src/main/java/com/sss/university/student/ms/feignclients/AddressFeignClient.java
+++ student-microservice/src/main/java/com/sss/university/student/ms/feignclients/AddressFeignClient.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.sss.university.student.ms.response.AddressResponse;
 
-@FeignClient(url="${address.service.url}",value="address-feign-client",path="/api/address-service")
+//@FeignClient(url="${address.service.url}",value="address-feign-client",path="/api/address-service")
+
+//Below Code uses Eureka-Discovery-Service for service registration and Feign Client refers to the service names of the micro-services than the URLs
+@FeignClient(value="address-microservice",path="/api/address-service")
 public interface AddressFeignClient {
 	@GetMapping("/getById/{id}")
 	public AddressResponse getById(@PathVariable long id);
