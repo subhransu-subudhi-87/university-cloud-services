81276408e235689432f4ec088071067ecca2aefd
subhransu-subudhi-87 <subhransu-subudhi87@gmail.com>
2022-03-03 20:53:04 +0530

    University-App Eureka-Discovery-Service Code Pushed

---
+++ university-discovery-service/src/test/java/com/sss/university/eureka/ms/UniversityDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sss.university.eureka.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
